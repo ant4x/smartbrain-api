d7ddf48522c641d322745351385679c994c8dca5
Anthony Fox <anton.airsound@gmail.com>
2022-11-05 01:27:44 +0300

    fixed minor issues

--- controllers/image.js
+++ controllers/image.js
@@ -1,14 +1,14 @@
 import Clarifai from "clarifai"
 
 const app = new Clarifai.App({
-    apiKey: "853a09fcb00b422880a34d3b21594cc5"
+    apiKey: process.env.KEY
 })
 
 const handleApiCall = (req, res) => {
     app.models
         .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
         .then(data => res.json(data))
-        .catch(err => res.status(400).json('unable to work with API'))
+        .catch(err => res.status(400).json("unable to work with API"))
 }
 
 const handleImage = (req, res, db) => {
