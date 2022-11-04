c2b75ef069a4144aea0a793091f6fa0750145ff2
Anthony Fox <anton.airsound@gmail.com>
2022-11-04 21:42:11 +0300

    testing ssl-related code

--- server.js
+++ server.js
@@ -14,11 +14,8 @@ dotenv.config()
 const db = knex({
     client: "pg",
     connection: {
-        host: "127.0.0.1",
-        port: 5432,
-        user: "postgres",
-        password: "123",
-        database: "smart-brain"
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
